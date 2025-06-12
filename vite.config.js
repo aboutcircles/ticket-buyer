765050f2fa7982f5d0714ec89be4b6422b5aaa40
Vanshika Srivastava <vanshikasrivastava.business@gmail.com>
2025-06-12 15:01:08 +0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/buy-dappcon-ticket-with-circles", // 👈 ADD THIS
+  base: "/ticket-buyer/",
   plugins: [react()],
   resolve: {
     alias: {
